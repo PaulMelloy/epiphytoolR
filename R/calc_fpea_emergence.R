adf84698c9863e28fc49ec2df9f6a0580740c3f9
Paul Melloy <paul.melloy@usq.edu.u>
2021-04-14 10:39:28 +1000

    change input to date and calibrate for hemisphere

--- R/calc_fpea_emergence.R
+++ R/calc_fpea_emergence.R
@@ -11,15 +11,32 @@
 #' @source <https://doi.org/10.1007/978-1-4020-6065-6_9>
 #'
 #'
-#' @param das number of days after sowing (integer)
+#' @param date number of days after sowing (integer)
 #'
 #' @return number of degree days predicted from time of sowing to emergence
 #' @export
 #'
 #' @examples
 #' calc_fpea_emergence(14)
-calc_fpea_emergence <- function(das){
-   x1 <- das/14
+calc_fpea_emergence <- function(sowing_date, hemisphere = "south"){
+
+   # calibrate the sowing date to suit the input for northern or southern hemisphere
+   # the Schoeny paper was in France and centered on 19/9/2003 8 days before the
+   # autumn equinox, hence the adjustment
+   if(hemisphere == "north" | "N" | "North"){
+      autumn_equinox <- 79 - 8
+   }else{
+      if(hemisphere == "south" | "S" | "South"){
+         autumn_equinox <- 226 - 8
+      }
+   }else{
+      stop("Can't understand 'hemisphere' input/n please input 'north' or 'south'." )
+   }
+
+   sowing_day <- lubridate::yday(as.Date(sowing_date))
+
+   x1 <- (autumn_equinox - sowing_day)/14
+
    y1 <- (-4.08 * (x1^2)) +
       (57.36 * x1) +
       97.49
