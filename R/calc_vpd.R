63f022f4693587012af80e3f54a87f14114dd184
PaulMelloy <paul@melloy.com.au>
2022-01-19 22:21:07 +1000

    Add 'eq'uarion options for VPD

--- R/calc_vpd.R
+++ R/calc_vpd.R
@@ -8,13 +8,23 @@
 #'
 #' @return Vapour-pressure deficit in KPa
 #' @references
+#'  https://doi.org/10.1175/1520-0450(1967)006<0203:OTCOSV>2.0.CO;2
 #' @export
+#'
 #'
 #' @examples
 #' calc_vpd()
 #' calc_vpd(RH = 99, Tm = 30)
-calc_vpd <- function(RH = 70, Tm = 24){
-   #SVP <- 610.7*10^((7.5*Tm)/237.3 + Tm)
+calc_vpd <- function(RH = 70, Tm = 24, eq = "Murray"){
+   if(eq == "Sapak"){
+      SVP <- 610.7*(10^((7.5*Tm)/237.3 + Tm))
+      return((1-(RH/100))*SVP)
+   }else{
+      # Murray 1967
+      SVP <- (10^(((Tm * 7.5)/(Tm + 237.3))+0.7858))*0.1
+      return((1-(RH/100))*SVP)
+   }
+
 
    #
    # SVP <- exp(17.27*Tm/(T+237.3))
@@ -22,8 +32,7 @@ calc_vpd <- function(RH = 70, Tm = 24){
    # https://physics.stackexchange.com/questions/4343/how-can-i-calculate-vapor-pressure-deficit-from-temperature-and-relative-humidit
    # SVP <- 6.1078*exp(17.2693882*(Tm-273.16))
    #
-   SVP <- (10^(((Tm * 7.5)/(Tm + 237.3))+0.7858))*0.1
-   (1-(RH/100))*SVP
+
 
    #(RH/100 * SVP) - SVP
 }
