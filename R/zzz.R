da2762a2c699019a03033671c16f49688d6a8ed3
PaulMelloy <paul@melloy.com.au>
2022-03-02 13:50:03 +1000

    import sf for lutz

--- R/zzz.R
+++ R/zzz.R
@@ -7,3 +7,13 @@ NULL
 `%notin%` <- function(x, table) {
    match(x, table, nomatch = 0L) == 0L
 }
+
+# This function is never called.
+# It only makes `sf` available in the package for `lutz` to use.
+# `lutz` only has `sf` in Suggests, but it is needed for the function we use
+# from `lutz`
+
+stub <- function() {
+   sf::st_as_sf()
+}
+# nocov end
