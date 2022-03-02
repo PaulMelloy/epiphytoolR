22f7823bec474557532843814bbe8cc6b38b6338
PaulMelloy <paul@melloy.com.au>
2022-03-02 12:52:38 +1000

    init test

---
+++ tests/testthat/test-calc_vpd.R
@@ -0,0 +1,8 @@
+test_that("calc_vpd returns expected output", {
+  expect_equal(calc_vpd(), 0.8949209,tolerance = 1e-06)
+   expect_equal(calc_vpd(RH = 96,18.8), 0.0867813,tolerance = 1e-06)
+   expect_equal(calc_vpd(RH = 70,
+                         Tm = 20), 0.7012911,tolerance = 1e-06)
+   expect_equal(calc_vpd(RH = 38.6,
+                         Tm = 30), 2.604469,tolerance = 1e-06)
+})
