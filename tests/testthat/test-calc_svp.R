e6938edb93c9d0734131a4c1dc08133805144d12
PaulMelloy <paul@melloy.com.au>
2022-03-16 12:42:25 +1000

    sp

--- tests/testthat/test-calc_svp.R
+++ tests/testthat/test-calc_svp.R
@@ -1,4 +1,4 @@
-test_that("calc_spd returns expected output", {
+test_that("calc_svp returns expected output", {
    expect_equal(calc_svp(25), 3.166871,tolerance = 1e-06)
    expect_equal(calc_svp(18.8), 2.169533,tolerance = 1e-06)
    expect_equal(calc_svp(Tm = 20),
