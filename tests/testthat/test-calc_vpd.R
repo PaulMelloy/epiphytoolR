11fded49ed814f5cd37632545e19e6f0defbd739
PaulMelloy <paul@melloy.com.au>
2022-03-16 12:44:47 +1000

    add arguments to function

--- tests/testthat/test-calc_vpd.R
+++ tests/testthat/test-calc_vpd.R
@@ -1,5 +1,5 @@
 test_that("calc_vpd returns expected output", {
-  expect_equal(calc_vpd(), 0.8949209,tolerance = 1e-06)
+  expect_equal(calc_vpd(RH = 90, Tm = 26), 0.336047,tolerance = 1e-06)
    expect_equal(calc_vpd(RH = 96,18.8), 0.0867813,tolerance = 1e-06)
    expect_equal(calc_vpd(RH = 70,
                          Tm = 20), 0.7012911,tolerance = 1e-06)
