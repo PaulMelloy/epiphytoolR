6461bcfdb952b48d1b5828affa663bcda8b6947a
PaulMelloy <paul@melloy.com.au>
2022-03-16 20:18:03 +1000

    add tests

--- tests/testthat/test-check_DS.R
+++ tests/testthat/test-check_DS.R
@@ -1,6 +1,12 @@
 # This function should check the time quality of a vector of times
 w1 <-
-   fread(system.file("extdata", "scaddan_weather.csv", package = "epiphytoolR"))
+   fread(system.file("extdata", "naddacs_weather.csv", package = "epiphytoolR"))
+w1[,Local.Time := lubridate::with_tz(Local.Time,tzone = "Australia/Adelaide")]
+w1[,Local.Time := c(seq(from = w1$Local.Time[1],
+                      to = w1$Local.Time[.N],
+                      by = 3600))]
+unique(format(w1[,Local.Time], format = "%Z"))
+fwrite(w1, file = "inst/extdata/naddacs_weather.csv")
 
 #### Delete after testing
 #x <- w1$Local.Time
@@ -21,4 +27,51 @@ test_that("check_DS returns expected output to simple inputs", {
                     by = 3600), # one hour in seconds
                 check_DS(w1$Local.Time))
    expect_s3_class(check_DS(w1$Local.Time),"POSIXct")
+})
+
+
+test_that("time vectors with no Daylight savings shift from non-DS location returns input vector",{
+   tv1 <- seq(
+      from = as.POSIXct("2020-12-25 01:00:00"),
+      to = as.POSIXct("2021-12-25 01:00:00"),
+      by = 3600
+   )
+   expect_equal(check_DS(tv1, time_zone = "Australia/Brisbane"),
+                lubridate::with_tz(tv1, tzone = "UTC"))
+
+})
+
+test_that("time vectors with no Daylight savings shift from DS location returns UTC vector",{
+   tv2 <- seq(
+      from = as.POSIXct("2020-12-25 01:00:00"),
+      to = as.POSIXct("2021-12-25 01:00:00"),
+      by = 3600
+   )
+
+   expect_equal(check_DS(tv2, time_zone = "Australia/Sydney"),
+                lubridate::with_tz(
+                   lubridate::with_tz(tv2, tzone = "Australia/Sydney"),
+                   tzone = "UTC"))
+   expect_equal(check_DS(tv2, time_zone = "Australia/Adelaide"),
+                lubridate::with_tz(
+                   lubridate::with_tz(tv2, tzone = "Australia/Adelaide"),
+                   tzone = "UTC"))
+
+})
+
+test_that("time vectors with Daylight savings shift returns a UTC time with
+          continuity",{
+   tv3 <- seq(
+      from = as.POSIXct("2020-12-25 01:00:00"),
+      to = as.POSIXct("2021-12-25 01:00:00"),
+      by = 3600
+   )
+   tv3 <- lubridate::force_tz(tv3, "Australia/Sydney")
+   any(is.na(tv3))
+
+   expect_equal(check_DS(tv3, time_zone = "Australia/Sydney"),
+                lubridate::with_tz(
+                   lubridate::with_tz(tv2, tzone = "Australia/Sydney"),
+                   tzone = "UTC"))
+
 })
