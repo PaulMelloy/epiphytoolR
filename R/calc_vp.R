e595bb0179cb3857481c2588c076a367d0c14657
PaulMelloy <paul@melloy.com.au>
2022-03-16 12:52:29 +1000

    correct example

--- R/calc_vp.R
+++ R/calc_vp.R
@@ -14,7 +14,7 @@
 #'  https://doi.org/10.1175/1520-0450(1967)006<0203:OTCOSV>2.0.CO;2
 #' @export
 #' @examples
-#' calc_vp(Tm = 30)
+#' calc_vp(RH = 99,Tm = 30)
 #' calc_vp(RH = 99,Tm = 30, eq = "Sapak)
 calc_vp <- function(RH, Tm, eq = "Murray"){
    return(calc_svp(Tm, eq = eq) * (RH/100))
