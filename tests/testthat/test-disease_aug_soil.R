0ed38f39fb67cf718bfa23e3be493a8e3269a6e9
PaulMelloy <paul@melloy.com.au>
2022-03-17 19:34:59 +1000

    acknowlege warnings in test

--- tests/testthat/test-disease_aug_soil.R
+++ tests/testthat/test-disease_aug_soil.R
@@ -1,4 +1,4 @@
 test_that("formula works", {
-   expect_equal(round(disease_aug_soil(50),6), 0.511935)
-   expect_equal(round(disease_aug_soil(500),6), 0.938405)
+   expect_warning(expect_equal(round(disease_aug_soil(50),6), 0.511935))
+   expect_warning(expect_equal(round(disease_aug_soil(500),6), 0.938405))
 })
