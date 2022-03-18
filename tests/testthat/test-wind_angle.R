a00d8a98f4d549f32c6811b3c059d3dc20442998
PaulMelloy <paul@melloy.com.au>
2022-03-18 13:23:07 +1000

    add minimum standard deviation to wind direction and tests

--- tests/testthat/test-wind_angle.R
+++ tests/testthat/test-wind_angle.R
@@ -1,6 +1,10 @@
 
-w_a <- wind_angle(1, 30, 1)
-w_a10 <- wind_angle(1, 30, 10)
+w_a <- wind_angle(mean_wind_direction = 1,
+                  stdev_wind_direction = 30,
+                  PSPH =  1)
+w_a10 <- wind_angle(mean_wind_direction = 1,
+                    stdev_wind_direction = 30,
+                    PSPH =  10)
 
 test_that("wind_angle returns a number between 0 and 360", {
 
@@ -26,5 +30,24 @@ test_that("wind_angle returns the correct class", {
   # test the class of output
   expect_true(is.numeric(w_a))
   expect_true(is.vector(w_a))
+
+})
+
+test_that("wind_angle has a minimum standard deviation", {
+   # test the class of output
+   sd1 <- wind_angle(
+      mean_wind_direction = 180,
+      stdev_wind_direction = 0.5,
+      PSPH =  10000,
+      min_stdev = 1
+   )
+   expect_equal(sd(sd1),1,tolerance = 0.005)
+   sd40 <- wind_angle(
+      mean_wind_direction = 180,
+      stdev_wind_direction = 30,
+      PSPH =  10000,
+      min_stdev = 40
+   )
+   expect_equal(sd(sd40),40,tolerance = 0.05)
 
 })
