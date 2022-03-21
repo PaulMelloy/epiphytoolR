362cffa56ae9cb1bb8c0d4d0ce0a0be0909c6910
PaulMelloy <paul@melloy.com.au>
2022-03-21 12:13:02 +1000

    add function which supports format_weather()

---
+++ R/check_weather_warnings.R
@@ -0,0 +1,22 @@
+#' Check format_weather warnings
+#'
+#' Print internal warnings produced during the running of format_weather
+#'
+#' @return lines of messages with the function, type (either a message
+#'  or warning) and the output of the message or warning
+#' @export
+#'
+#' @examples
+#' f <- function(x) {
+#'    warning("This is a warning")
+#'    message("This is a message")
+#'    x + x}
+#' blackspot.sp::.capture_warnings(f(1))
+#' check_weather_warnings()
+check_weather_warnings <- function() {
+   invisible(lapply(warn$captured_warnings, function(x) {
+      x <- unique(unlist(x))
+      message(paste0(x, sep = " - "))
+
+   }))
+}
