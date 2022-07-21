bacce13fe45d88c680d90638c59a1b6cff5f8249
PaulMelloy <paul@melloy.com.au>
2022-07-21 13:43:02 +0800

    add formula two draft

--- R/exp_dk.R
+++ R/exp_dk.R
@@ -17,3 +17,43 @@
 exp_dk <- function(r, a = 1) {
    (exp(-r / a) / (2 * pi * (a ^ 2)))
 }
+
+#' Function two in Karisto et al. (2022)
+#'
+#' @param initial_intensity
+#' @param transmission_rate
+#' @param dist
+#' @param measurement_width
+#' @param border
+#' @param source_width
+#' @param destination_width
+#' @param source_length
+#' @param a
+#'
+#' @return
+#'
+#' @examples
+f2 <- function(initial_intensity,
+               transmission_rate = 2,
+               dist,
+               measurement_width = 10,
+               border = 12.5,
+               source_width = 40,
+               destination_width = 10,
+               source_length = 112.5,
+               a = 1){
+
+   width <- source_length
+   area <-
+      (initial_intensity * transmission_rate)/
+      (width * 2*border)
+
+   area *
+      (exp(-(
+         sqrt(
+            (dist - source_width)^2 +
+            (measurement_width - source_length)^2)/
+            a))/
+          2*pi*a^2
+      )
+}
