9c873db5bcede45d8dcda4e0c28a067b69239a3a
PaulMelloy <paul@melloy.com.au>
2022-08-11 10:47:38 +1000

    update function two in exp_dk add description to pass checks

--- R/exp_dk.R
+++ R/exp_dk.R
@@ -20,23 +20,23 @@ exp_dk <- function(r, a = 1) {
 
 #' Function two in Karisto et al. (2022)
 #'
-#' @param initial_intensity
-#' @param transmission_rate
-#' @param dist
-#' @param measurement_width
-#' @param border
-#' @param source_width
-#' @param destination_width
-#' @param source_length
-#' @param a
+#' @param initial_intensity numeric, Initial inoculum intensity
+#' @param transmission_rate numeric, rate at which spores are produced and dispersed
+#' @param dist numeric, distance from source in which to query the inoculum risk
+#' @param border the border, needs description
+#' @param source_width numeric, width of the inoculum source in centimetres
+#' @param destination_width numeric, width in centimetres
+#' @param source_length numeric, length of the inoculum source in centimetres
+#' @param a numeric, exponential dispersal kernel constant
 #'
-#' @return
+#' @return numeric, the number of spores estimated to have dispersed to the defined
+#'  location
 #'
 #' @examples
+#' @noRd
 f2 <- function(initial_intensity,
                transmission_rate = 2,
                dist,
-               measurement_width = 10,
                border = 12.5,
                source_width = 40,
                destination_width = 10,
@@ -52,7 +52,7 @@ f2 <- function(initial_intensity,
       (exp(-(
          sqrt(
             (dist - source_width)^2 +
-            (measurement_width - source_length)^2)/
+            (destination_width - source_length)^2)/
             a))/
           2*pi*a^2
       )
