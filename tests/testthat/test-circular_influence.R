a5abd146e3b23378fe484f440f83fecd91d014df
PaulMelloy <paul@melloy.com.au>
2022-08-11 10:48:23 +1000

    Update tests based on new outputs

--- tests/testthat/test-circular_influence.R
+++ tests/testthat/test-circular_influence.R
@@ -2,25 +2,27 @@ test_that("circ_influence tests work", {
    # Wind speed strength from a northerly wind
    ws <- 5 # kph
    wd <- 0 # degrees
-   expect_equal(ws * circular_influence(wd),ws)
+   expect_equal(ws * circular_influence(wd), c(x = 0,
+                                               y = ws))
 
    # Wind speed strength from a north east wind
    wd <- 45 # degrees
-   expect_equal(ws * circular_influence(wd),ws * 0.5)
+   expect_equal(ws * circular_influence(wd),
+                c(x = sin((wd/360) *(2*pi))*ws,
+                  y = cos((wd/360) *(2*pi))*ws),
+                tolerance = 0.0001
+                  )
 
    # Wind speed strength from a south west wind
    wd <-225 # degrees
-   expect_equal(ws * circular_influence(wd),ws * -0.5)
+   expect_equal(ws * circular_influence(wd),
+                c(x = sin((wd/360) *(2*pi))*ws,
+                  y = cos((wd/360) *(2*pi))*ws),
+                tolerance = 0.0001
+   )
 
 })
 
-test_that("circ_influence vector inputs work", {
-   # Wind speed strength from a northerly wind
-   ws <- 5 # kph
-   wd <- 1:360 # degrees
-   expect_length(ws * circular_influence(wd),360)
-   expect_equal(sum(ws * circular_influence(wd)),0)
-})
 
 test_that("circ_influence stops expectantly", {
    expect_error(circular_influence(361),regexp = "'x' must be a number between 0 and 360")
@@ -28,24 +30,18 @@ test_that("circ_influence stops expectantly", {
 })
 
 test_that("circ_influence offset works", {
-   expect_equal(sum(circular_influence(x= 1:360, offset = 10)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 25)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 65)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 90)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 120)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 180)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 200)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 225)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = 290)),0)
+   expect_equal(circular_influence(x= 1, offset = 10),
+                circular_influence(x= 351),
+                tolerance = 0.00001)
+   expect_equal(circular_influence(x= 360, offset = 25),
+                circular_influence(x= 360 - 25),
+                tolerance = 0.00001)
 
-   expect_equal(sum(circular_influence(x= 1:360, offset = -10)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = -25)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = -65)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = -90)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = -120)),0)
-   expect_equal(sum(circular_influence(x= 1:360, offset = -180)),0)
-   #expect_equal(sum(circular_influence(x= 1:360, offset = -200)),0)
-   #expect_equal(sum(circular_influence(x= 1:360, offset = -225)),0)
-   #expect_equal(sum(circular_influence(x= 1:360, offset = -290)),0)
+   expect_equal(circular_influence(x= 1, offset = -10),
+                circular_influence(x= 11),
+                tolerance = 0.00001)
+   expect_equal(circular_influence(x= 360, offset = -25),
+                circular_influence(x= 0 + 25),
+                tolerance = 0.00001)
 
 })
