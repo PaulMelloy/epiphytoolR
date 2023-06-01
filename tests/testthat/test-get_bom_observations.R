adc0ef0a49ab9492acc8934a8c8d7bf729e1d789
PaulMelloy <paul@melloy.com.au>
2023-06-01 11:54:52 +1000

    passing all checks

--- tests/testthat/test-get_bom_observations.R
+++ tests/testthat/test-get_bom_observations.R
@@ -1,7 +1,9 @@
 test_that("bom download works", {
+   dl_location <- tempdir()
   dl_loc <-
      get_bom_observations(ftp_url = "ftp://ftp.bom.gov.au/anon/gen/fwo/",
-                          download_location = tempdir())
+                          download_location = dl_location,
+                          access_warning = FALSE)
 
   dat <-
      merge_axf_weather(File_compressed = dl_loc,
