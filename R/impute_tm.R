af644724a718a9e411f1e040e5609f0d61afd197
PaulMelloy <paul@melloy.com.au>
2023-07-13 08:57:51 +1000

    add a vector input of either time or integers

--- R/impute_tm.R
+++ R/impute_tm.R
@@ -3,7 +3,9 @@
 #' @details
 #' Impute hourly temperatures from daily maximums and minimums
 #'
-#'
+#' @param x numeric or POSIX, vector. numeric vectors indicate which hour to
+#'  return. A POSIX formatted vector can also be used and the imputed temperature
+#'  for the hour returned.
 #' @param max_tm numeric, maximum daily temperature
 #' @param min_tm numeric, minimum daily temperature
 #' @param max_hour integer, hour in the day when maximum temperature occurred
@@ -14,12 +16,13 @@
 #'
 #' @examples
 #' impute_tm()
+#' impute_tm(Sys.time())
 #' impute_tm(max_tm = 22,
 #'           min_tm = 18)
 #' impute_tm(max_hour = 10,
 #'           min_hour = 1)
 
-impute_tm <- function(max_tm = 28, min_tm = 10, max_hour = 14, min_hour = 5){
+impute_tm <- function(x = 1:24, max_tm = 28, min_tm = 10, max_hour = 14, min_hour = 5){
 
    if(max_tm < min_tm)stop("'min_tm': ",min_tm,
                            " is larger than 'max_tm': ", max_tm)
@@ -41,6 +44,17 @@ impute_tm <- function(max_tm = 28, min_tm = 10, max_hour = 14, min_hour = 5){
    day_hourly[1:(max_hour-1)] <- tm_hourly[(min_ind - (min_hour)+1):24]
    day_hourly[(max_hour):24] <- tm_hourly[1:(min_ind - min_hour)]
 
-   return(day_hourly)
-
+   if(inherits(x, "numeric") |
+      inherits(x, "integer")){
+      h_ind <- x %% 24
+      h_ind[h_ind == 0] <- 24
+      return(day_hourly[h_ind])
+   }
+   if(inherits(x, "POSIXt")){
+      h_ind <- data.table::hour(x) %% 24
+      h_ind[h_ind == 0] <- 24
+      return(day_hourly[h_ind])
+   }else{
+      stop("'x' should be a numeric or POSIXt vector")
+   }
 }
